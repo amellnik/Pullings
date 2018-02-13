13fcee00b4bd71005b64b860f50e106b39514204
Alex Mellnik <a.r.mellnik@gmail.com>
2018-02-12 21:39:33 -0800

    Add CORS

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 3000
 var app = express();
 app.use(bodyParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
