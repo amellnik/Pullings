91dbe3f064d63f21f8e4d8cb0a6910136b354873
Alex Mellnik <a.r.mellnik@gmail.com>
2018-02-12 21:42:20 -0800

    Get origin from HOST

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(bodyParser());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.origin);
+  res.header("Access-Control-Allow-Origin", rreq.get('host'));
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
