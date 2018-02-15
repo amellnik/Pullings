6cf1f5904043421db53f2ab0187e85a05da0dc9b
Alex Mellnik <a.r.mellnik@gmail.com>
2018-02-14 21:04:32 -0800

    Add mysql connection

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var mysql      = require('mysql');
 
 const path = require('path')
 const PORT = process.env.PORT || 3000
+
+var pool = mysql.createPool({
+  host     : process.env.MYSQL_SERVER,
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASS,
+  database : 'aubrey_api'
+});
 
 var app = express();
 app.use(bodyParser());
@@ -19,7 +27,20 @@ app.get('/', (req, res) => res.json({error: false, message: 'Nothing to see here
 // POST method route
 app.post('/', function (req, res) {
   console.log(req.body);
-  res.json({error: false});
+  pool.getConnection(function(err, connection) {
+    // Use the connection
+    connection.query('INSERT INTO demo VALUES (NULL, ?, ?, ?)', [req.body.lat, req.body.long, req.body.text], function (error, results, fields) {
+      // And done with the connection.
+      connection.release();
+
+
+      if (error) {
+        res.json({error: true, message: error});
+      };
+      res.json({error: false});
+    });
+  });
+
 });
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT));
